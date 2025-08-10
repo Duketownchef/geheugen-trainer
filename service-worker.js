3c68c2ee6d6110742ffe111dc63edfc207f45acc
Duketownchef <yvoschreuder@live.nl>
2025-08-10 08:59:52 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+
+const CACHE_NAME = 'geheugen-trainer-v2';
+const ASSETS = [
+  './geheugen-trainer-pwa.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        if (req.method === 'GET' && res && res.status === 200 && res.type === 'basic') {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone));
+        }
+        return res;
+      }).catch(() => {
+        if (req.headers.get('accept')?.includes('text/html')) {
+          return caches.match('./geheugen-trainer-pwa.html');
+        }
+      });
+    })
+  );
+});
